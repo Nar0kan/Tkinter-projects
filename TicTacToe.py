271798efcd65dd4a70cbee9570f1998648866da3
Nar0kan <wertynhard@gmail.com>
2021-03-18 21:12:22 +0200

    V0.6

--- TicTacToe.py
+++ TicTacToe.py
@@ -6,99 +6,50 @@ root.geometry(f"400x480")
 
 #Functions:
 t = 0
-def x1(button):
-    global t
+lst = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+def sign(k):
+    global t, lst
     if t % 2 == 0:
         button = Button(text="X", padx=60, pady=50)
+        lst[k-1] = "x"
     else:
         button = Button(text="0", padx=60, pady=50)
-    button.grid(column=0, row=0)
-    t += 1
-    return button
-def x2(button):
-    global t
-    if t % 2 == 0:
-        button = Button(text="X", padx=60, pady=50)
+        lst[k-1] = "0"
+    if k == 1:
+        button.grid(column=0, row=0)
+    elif k == 2:
+        button.grid(column=1, row=0)
+    elif k == 3:
+        button.grid(column=2, row=0)
+    elif k == 4:
+        button.grid(column=0, row=1)
+    elif k == 5:
+        button.grid(column=1, row=1)
+    elif k == 6:
+        button.grid(column=2, row=1)
+    elif k == 7:
+        button.grid(column=0, row=2)
+    elif k == 8:
+        button.grid(column=1, row=2)
     else:
-        button = Button(text="0", padx=60, pady=50)
-    button.grid(column=1, row=0)
+        button.grid(column=2, row=2)
     t+=1
-    return button
-def x3(button):
-    global t
-    if t % 2 == 0:
-        button = Button(text="X", padx=60, pady=50)
-    else:
-        button = Button(text="0", padx=60, pady=50)
-    button.grid(column=2, row=0)
-    t+=1
-    return button
-def x4(button):
-    global t
-    if t % 2 == 0:
-        button = Button(text="X", padx=60, pady=50)
-    else:
-        button = Button(text="0", padx=60, pady=50)
-    button.grid(column=0, row=1)
-    t+=1
-    return button
-def x5(button):
-    global t
-    if t % 2 == 0:
-        button = Button(text="X", padx=60, pady=50)
-    else:
-        button = Button(text="0", padx=60, pady=50)
-    button.grid(column=1, row=1)
-    t+=1
-    return button
-def x6(button):
-    global t
-    if t % 2 == 0:
-        button = Button(text="X", padx=60, pady=50)
-    else:
-        button = Button(text="0", padx=60, pady=50)
-    button.grid(column=2, row=1)
-    t+=1
-    return button
-def x7(button):
-    global t
-    if t % 2 == 0:
-        button = Button(text="X", padx=60, pady=50)
-    else:
-        button = Button(text="0", padx=60, pady=50)
-    button.grid(column=0, row=2)
-    t+=1
-    return button
-def x8(button):
-    global t
-    if t % 2 == 0:
-        button = Button(text="X", padx=60, pady=50)
-    else:
-        button = Button(text="0", padx=60, pady=50)
-    button.grid(column=1, row=2)
-    t+=1
-    return button
-def x9(button):
-    global t
-    if t % 2 == 0:
-        button = Button(text = "X", padx = 60, pady = 50)
-    else:
-        button = Button(text = "0", padx = 60, pady = 50)
-    button.grid(column = 2, row = 2)
-    t+=1
+    print(check())
     return button
 
 def clear():
     global button_1, button_2, button_3, button_4, button_5, button_6, button_7, button_8, button_9
-    button_1 = Button(text=" ", padx=60, pady=50, bd=2, command=lambda: x1(button_1))
-    button_2 = Button(text=" ", padx=60, pady=50, bd=2, command=lambda: x2(button_2))
-    button_3 = Button(text=" ", padx=60, pady=50, bd=2, command=lambda: x3(button_3))
-    button_4 = Button(text=" ", padx=60, pady=50, bd=2, command=lambda: x4(button_4))
-    button_5 = Button(text=" ", padx=60, pady=50, bd=2, command=lambda: x5(button_5))
-    button_6 = Button(text=" ", padx=60, pady=50, bd=2, command=lambda: x6(button_6))
-    button_7 = Button(text=" ", padx=60, pady=50, bd=2, command=lambda: x7(button_7))
-    button_8 = Button(text=" ", padx=60, pady=50, bd=2, command=lambda: x8(button_8))
-    button_9 = Button(text=" ", padx=60, pady=50, bd=2, command=lambda: x9(button_9))
+    global t, lst
+    button_1 = Button(text=" ", padx=60, pady=50, bd=3, command=lambda: sign(1))
+    button_2 = Button(text=" ", padx=60, pady=50, bd=3, command=lambda: sign(2))
+    button_3 = Button(text=" ", padx=60, pady=50, bd=3, command=lambda: sign(3))
+    button_4 = Button(text=" ", padx=60, pady=50, bd=3, command=lambda: sign(4))
+    button_5 = Button(text=" ", padx=60, pady=50, bd=3, command=lambda: sign(5))
+    button_6 = Button(text=" ", padx=60, pady=50, bd=3, command=lambda: sign(6))
+    button_7 = Button(text=" ", padx=60, pady=50, bd=3, command=lambda: sign(7))
+    button_8 = Button(text=" ", padx=60, pady=50, bd=3, command=lambda: sign(8))
+    button_9 = Button(text=" ", padx=60, pady=50, bd=3, command=lambda: sign(9))
     button_1.grid(column=0, row=0)
     button_2.grid(column=1, row=0)
     button_3.grid(column=2, row=0)
@@ -108,16 +59,95 @@ def clear():
     button_7.grid(column=0, row=2)
     button_8.grid(column=1, row=2)
     button_9.grid(column=2, row=2)
+    t = 0
+    lst = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
-button_1 = Button(text = " ", padx = 60, pady = 50, bd = 2, command = lambda: x1(button_1))
-button_2 = Button(text = " ", padx = 60, pady = 50, bd = 2, command = lambda: x2(button_2))
-button_3 = Button(text = " ", padx = 60, pady = 50, bd = 2, command = lambda: x3(button_3))
-button_4 = Button(text = " ", padx = 60, pady = 50, bd = 2, command = lambda: x4(button_4))
-button_5 = Button(text = " ", padx = 60, pady = 50, bd = 2, command = lambda: x5(button_5))
-button_6 = Button(text = " ", padx = 60, pady = 50, bd = 2, command = lambda: x6(button_6))
-button_7 = Button(text = " ", padx = 60, pady = 50, bd = 2, command = lambda: x7(button_7))
-button_8 = Button(text = " ", padx = 60, pady = 50, bd = 2, command = lambda: x8(button_8))
-button_9 = Button(text = " ", padx = 60, pady = 50, bd = 2, command = lambda: x9(button_9))
+def check():
+    global lst, t, h
+    if lst[0] == lst[1] == lst[2]:
+        h = 1
+        change_color(0, 1, 2)
+        return "Game over!"
+    elif lst[3] == lst[4] == lst[5]:
+        h = 2
+        change_color(3, 4, 5)
+        return "Game over!"
+    elif lst[6] == lst[7] == lst[8]:
+        h = 3
+        change_color(6, 7, 8)
+        return "Game over!"
+    elif lst[0] == lst[3] == lst[6]:
+        h = 4
+        change_color(0, 3, 6)
+        return "Game over!"
+    elif lst[1] == lst[4] == lst[7]:
+        h = 5
+        change_color(1, 4, 7)
+        return "Game over!"
+    elif lst[2] == lst[5] == lst[8]:
+        h = 6
+        change_color(2, 5, 8)
+        return "Game over!"
+    elif lst[0] == lst[4] == lst[8]:
+        h = 7
+        change_color(0, 4, 8)
+        return "Game over!"
+    elif lst[2] == lst[4] == lst[6]:
+        h = 8
+        change_color(2, 4, 6)
+        return "Game over!"
+    elif t >= 9:
+        clear()
+        return "Friendship won!"
+    else:
+        pass
+def change_color(k, l, m):
+    global lst, h
+    btn1 = Button(text = lst[k], padx = 60, pady = 50, bd = 2, bg = "yellow")
+    btn2 = Button(text = lst[l], padx = 60, pady = 50, bd = 2, bg = "yellow")
+    btn3 = Button(text = lst[m], padx = 60, pady = 50, bd = 2, bg = "yellow")
+    if h == 1:
+        btn1.grid(column=0, row=0)
+        btn2.grid(column=1, row=0)
+        btn3.grid(column=2, row=0)
+    elif h == 2:
+        btn1.grid(column=0, row=1)
+        btn2.grid(column=1, row=1)
+        btn3.grid(column=2, row=1)
+    elif h == 3:
+        btn1.grid(column=0, row=2)
+        btn2.grid(column=1, row=2)
+        btn3.grid(column=2, row=2)
+    elif h == 4:
+        btn1.grid(column=0, row=0)
+        btn2.grid(column=0, row=1)
+        btn3.grid(column=0, row=2)
+    elif h == 5:
+        btn1.grid(column=1, row=0)
+        btn2.grid(column=1, row=1)
+        btn3.grid(column=1, row=2)
+    elif h == 6:
+        btn1.grid(column=2, row=0)
+        btn2.grid(column=2, row=1)
+        btn3.grid(column=2, row=2)
+    elif h == 7:
+        btn1.grid(column=0, row=0)
+        btn2.grid(column=1, row=1)
+        btn3.grid(column=2, row=2)
+    else:
+        btn1.grid(column=0, row=2)
+        btn2.grid(column=1, row=1)
+        btn3.grid(column=2, row=0)
+# Buttons
+button_1 = Button(text = " ", padx = 60, pady = 50, bd = 2, command = lambda: sign(1))
+button_2 = Button(text = " ", padx = 60, pady = 50, bd = 2, command = lambda: sign(2))
+button_3 = Button(text = " ", padx = 60, pady = 50, bd = 2, command = lambda: sign(3))
+button_4 = Button(text = " ", padx = 60, pady = 50, bd = 2, command = lambda: sign(4))
+button_5 = Button(text = " ", padx = 60, pady = 50, bd = 2, command = lambda: sign(5))
+button_6 = Button(text = " ", padx = 60, pady = 50, bd = 2, command = lambda: sign(6))
+button_7 = Button(text = " ", padx = 60, pady = 50, bd = 2, command = lambda: sign(7))
+button_8 = Button(text = " ", padx = 60, pady = 50, bd = 2, command = lambda: sign(8))
+button_9 = Button(text = " ", padx = 60, pady = 50, bd = 2, command = lambda: sign(9))
 button_refresh = Button(text = "Try again!", padx = 100, pady = 40, command = lambda: clear())
 button_refresh.grid(column = 0, row = 3, columnspan = 3)
 button_1.grid(column = 0, row = 0)
