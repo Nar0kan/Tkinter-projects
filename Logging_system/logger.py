cdd116ece39b2eac9f8123d01639f30c22480e32
Nar0kan <wertynhard@gmail.com>
2021-07-24 15:39:39 +0300

    OOP instead of functional

--- Logging_system/logger.py
+++ Logging_system/logger.py
@@ -19,44 +19,39 @@ class Verification:
         else:
             self.login_user()
 
+
     @staticmethod
-    def check_user(user_info):
-        # open file
+    def _check_user(user_info):
+        # open file and write lines to a list (log_list)
         loggins = open('loggins.txt', 'r')
-        k_list = list(loggins.readlines())
+        log_list = list(loggins.readlines())
         loggins.close()
-        print(k_list)
         
-        for couple in range(1, len(k_list)):
-           # print(k_list[couple])
-            print(user_info)
-            if str(user_info[0]) == str(k_list[couple-1]) and str(user_info[1]) == str(k_list[couple]):
+        # search for nickname and password in saved list (log_list)
+        for couple in range(0, round(len(log_list)/2), 2):
+            if str(user_info[0]) == str(log_list[couple]) and str(user_info[1]) == str(log_list[couple+1]):
                 return True
         
         return False
         
 
     def login_user(self):
-        
-        if self.check_user(self._user_info):
-            messagebox.showinfo(title = 'Success', message = 'You are logged in as ' + self._user_info[0])
 
-            # feature to show result on Main program window
-            #My_label['text'] = " Hello, " + self._user_info[0]
-            #My_label.grid(row = 2, column = 0, rowspan = 5)  
+        if self._check_user(self._user_info):
+            messagebox.showinfo(title = 'Success', message = 'You are logged in as ' + self._user_info[0])
+            # here implement function to show that user is logged in and load logged menu
         else:
             messagebox.showinfo(title = 'Problem', message = 'You have entered wrong password or login!')
-            self.on_cancel(self._top)
-        
+            self._on_cancel(self._top)
         
         return self._top.destroy()
 
     
     def create_account(self):
         # check if user exist
-        if self.check_user(self._user_info):
+        if self._check_user(self._user_info):
             messagebox.showerror(message = 'This user already exist.')
-            self.on_cancel(self._top)
+            self._on_cancel(self._top)
             return None
         else:
             # and now we can add new user in our file
@@ -65,39 +60,40 @@ class Verification:
             loggins.writelines("".join(self._user_info))
             loggins.close()
 
-        self.on_cancel(self._top)
+        self._on_cancel(self._top)
+
 
     # def for cancel button
     @staticmethod
-    def on_cancel(top):
+    def _on_cancel(top):
         top.destroy()
         log_button['state'] = NORMAL
         sign_up_button['state'] = NORMAL
-        
 
 
-# some functions for login/sign up
+# class for login/sign up
 class Not_logged_Menu(Verification):
     def __init__(self, option):
         if option == "login":
             self.login()
         else:
             self.sign_up()
-        pass
+
 
     @staticmethod
-    def set_buttons_state_disabled():
+    def _set_buttons_state_disabled():
         log_button['state'] = DISABLED
         sign_up_button['state'] = DISABLED
 
-    
-    def login(self):
-        self.set_buttons_state_disabled()
-        
+
+    def _geometry(self):
+        #from tkinter import TopLevel
         top = Toplevel(root)
         top.title('Login window')
         top.geometry(f"470x130")
-        log_label = Label(top, text = '\n\nNickname \n \n \n \nPassword ', fg = 'pink', bg = 'black', height = 220, width = 300)
+
+        log_label = Label(top, text = '\n\nNickname \n \n \n \nPassword ', fg = 'pink',
+                         bg = 'black', height = 220, width = 300)
         log_label.grid(columnspan = 3, rowspan = 3)
 
         nickname = Text(log_label, width = 25, height = 1, pady = 5)
@@ -105,35 +101,45 @@ class Not_logged_Menu(Verification):
         nickname.grid(row = 2, column = 2, pady = 13, padx = 5)
         password.grid(row = 4, column = 2, pady = 13, padx = 5)
 
+        return top, log_label, nickname, password
+
+    
+    def login(self):
+        #build geometry for new login window
+        self._set_buttons_state_disabled()
+        top, log_label, nickname, password = self._geometry()
+
         confirm_button = Button(log_label, width = 15, height = 2, text = "Confirm",
-                                command = lambda: Verification((nickname.get(1.0, 16.0), password.get(1.0, 20.0)), top, 'login'))
+                                command = lambda: Verification((nickname.get(1.0, 16.0), password.get(1.0, 20.0)), 
+                                top, 'login'))
         confirm_button.grid(row = 4, column = 1, pady = 15, padx = 5)
 
-        cancel_button = Button(log_label, width=15, height=2, text="Cancel", command=lambda: self.on_cancel(top))
+        cancel_button = Button(log_label, width=15, height=2, text="Cancel", command=lambda: self._on_cancel(top))
         cancel_button.grid(row=4, column=3, pady=15, padx=5)
 
 
     def sign_up(self):
-        self.set_buttons_state_disabled()
-        
-        top = Toplevel(root)
-        top.title('Sign-up window')
-        top.geometry(f"470x130")
-        
-        su_label = Label(top, text='\n\nNickname \n \n \n \nPassword ', fg='pink', bg='black', height=220, width=300)
-        su_label.grid(columnspan=3, rowspan=3)
-
-        nickname = Text(su_label, width=25, height=1, pady=5)
-        password = Text(su_label, width=25, height=1, pady=5)
-        nickname.grid(row=2, column=2, pady=13, padx=5)
-        password.grid(row=4, column=2, pady=13, padx=5)
+        # build geometry for new sign up window
+        self._set_buttons_state_disabled()
+        top, su_label, nickname, password = self._geometry()
 
         confirm_button = Button(su_label, width=15, height=2, text="Confirm",
-                                command=lambda: Verification((nickname.get(1.0, 16.0), password.get(1.0, 20.0)), top, 'sign_up'))
+                                command=lambda: Verification((nickname.get(1.0, 16.0), password.get(1.0, 20.0)),
+                                 top, 'sign_up'))
         confirm_button.grid(row=4, column=1, pady=15, padx=5)
 
-        cancel_button = Button(su_label, width=15, height=2, text="Cancel", command=lambda: self.on_cancel(top))
+        cancel_button = Button(su_label, width=15, height=2, text="Cancel", command=lambda: self._on_cancel(top))
         cancel_button.grid(row=4, column=3, pady=15, padx=5)
+
+
+class Logged_Menu(Verification):
+    def __init__(self):
+        self._visual()
+    
+    @staticmethod
+    def _visual():
+        pass
+    
 
 #Objects
 def option(option):
@@ -145,7 +151,7 @@ My_label.grid(row = 2, column = 0, rowspan = 5)
 log_button = Button(root, text = " Login ", padx = 20, pady = 10, fg = 'pink',
                     bg = 'black', command = lambda: option('login'))
 sign_up_button = Button(root, text = " Sign up ", padx = 20, pady = 10, fg = 'pink',
-                        bg = 'black', command = lambda: option('sign_up'))
+                    bg = 'black', command = lambda: option('sign_up'))
 
 log_button.grid(row = 1, column = 5, padx = 200, pady = 5)
 sign_up_button.grid(row = 0, column = 5, padx = 200, pady = 10)
